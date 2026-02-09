da77037eba65a8bae17f02e1c11cb6a2316fa7dd
Cllaude99 <xodbsrla1025@naver.com>
2026-02-10 01:34:57 +0900

    refactor: InputGroup 컴포넌트의 size prop을 필수로 변경하여 일관성 향상
    - InputGroup 컴포넌트의 size prop을 선택적에서 필수로 변경하여 사용 시 명확성을 높였습니다.
    - 코드의 일관성을 유지하기 위해 size prop의 기본값을 제거했습니다.

--- packages/ui/src/components/Input/InputGroup/index.tsx
+++ packages/ui/src/components/Input/InputGroup/index.tsx
@@ -5,10 +5,10 @@ import type { InputSize } from '../types';
 import * as S from './InputGroup.styles';
 
 export interface InputGroupProps extends HTMLAttributes<HTMLDivElement> {
-  size?: InputSize;
+  size: InputSize;
 }
 
-const InputGroup = ({ children, size = 'medium', ...rest }: InputGroupProps) => {
+const InputGroup = ({ children, size, ...rest }: InputGroupProps) => {
   return (
     <InputProvider value={{ size }}>
       <S.Group role="group" {...rest}>
